259afaa924ba52b2ad375e653abe7ab8d543a200
Thaquidheen M A <thaquidheen@gmail.com>
2023-09-14 17:29:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Customers from './pages/customers/Customers';
 import Home from './pages/home/Home';
 import Invoice from './pages/invoicegenerate/Invoice';
 import Login from './pages/login/Login';
+import Report from './pages/report/Report';
 import Stock from './pages/stock/Stock';
 
 function App() {
   return (
     <div className="App">
-      <AddItem/>
+      <Report/>
     </div>
   );
 }
---
+++ src/pages/report/Report.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './report.css'
+const Report = () => {
+
+  return (
+    <div className="report-container">
+
+  </div>
+
+  )
+}
+
+export default Report
